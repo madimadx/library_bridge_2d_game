b0d28618d0c631d991238ecf686119b7b96d4dd9
Madison Maddox <madimadx@168.211.21.198.tigernet.wifi.dyn.clemson.edu>
2018-04-06 04:01:19 -0400

    updated graphics + paper

--- engine.cpp
+++ engine.cpp
@@ -38,11 +38,11 @@ Engine::Engine() :
   makeVideo( false )
 {
   srand(time(NULL));
-  sprites.emplace_back(new Sprite("RunningMan"));
-  sprites.emplace_back(new MultiSprite("RunningMan2R"));
+  //sprites.emplace_back(new Sprite("RunningMan"));
+  //sprites.emplace_back(new MultiSprite("RunningMan2R"));
   sprites.emplace_back(new MultiSprite("Student"));
   for (int i = 0; i < 7; i++) {
-    sprites.emplace_back(new MultiSprite2way("RunningMan2way", float(rand()%350), float(rand()%400)));
+    sprites.emplace_back(new MultiSprite("Paper", float(rand()%350), float(rand()%400)));
   }
   //Viewport::getInstance().setObjectToTrack(star);
   Viewport::getInstance().setObjectToTrack(sprites[currentSprite]);
